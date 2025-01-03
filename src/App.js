6e3fdf252c3996c8a3f91272eb2e9b0c949efd59
Souleymane DIALLO <sdley2020@gmail.com>
2025-01-03 16:41:50 +0000

    Restructuration de l'app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import { useState } from 'react';
 import './App.css';
-import Item from "./components/Item/Item";
-import List from './components/List/List';
-import { products } from './data/products';
+import Home from './pages/Home';
 
 function App() {
 
-  const [favorites, setFavorites] = useState([])
-  const handleFavorite = (id) => {
-    if (favorites.includes(id)) {
-      const othersId = favorites.filter(idProd => idProd !== id);
-      setFavorites(othersId);
-    } else {
-      setFavorites([...favorites, id]);
-    }
-  }
+  
 
 
   return (
     
     <div className='App'>
-      <List>
-        {
-          products.map(prod => (
-            <Item 
-              key={prod.id} 
-              product = {prod} 
-              favorite={favorites.includes(prod.id)}
-              onFavorite={handleFavorite}
-            />
-          ))
-        }
-      </List>
+      <Home />
     </div>
   );
 }
